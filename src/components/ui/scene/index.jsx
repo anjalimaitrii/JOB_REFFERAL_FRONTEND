5f2248a425825a13b4376ea9b9ab2c5335b510e0
anjalijangid <anjali.jangid@maitriiinfotech.com>
2026-03-09 16:52:44 +0530

    update

--- src/components/ui/scene/index.jsx
+++ src/components/ui/scene/index.jsx
@@ -3,7 +3,7 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
-import Model from "./model.jsx";
+import Model from "./model";
 
 export default function Index() {
   return (
